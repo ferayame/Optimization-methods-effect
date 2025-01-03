e9b0406395dc506d1e53743784d8614d05e38a05
Feriel080 <aourferiel22@outlook.com>
2025-01-03 15:36:48 +0100

    update

--- algorithms/MICE_RF.py
+++ algorithms/MICE_RF.py
@@ -3,7 +3,7 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.ensemble import RandomForestRegressor
 
-def imputation_with_mice_rf(data, filename):
+def imputation(data, filename):
     imputer = IterativeImputer(estimator=RandomForestRegressor(), max_iter=10, random_state=0)
     imputed_data = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
     fileDirectory = f"output/Imputed_{filename}_using_MICE.csv"
