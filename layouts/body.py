73eb66e5d67f72078f90a0befc85b781d2da7f1a
Feriel080 <aourferiel22@outlook.com>
2025-01-03 15:39:56 +0100

    update

--- layouts/body.py
+++ layouts/body.py
@@ -1,6 +1,6 @@
 from dash import html, dash_table, dcc
 import pandas as pd
-from algorithms import KNN_MLP, GA, MICE_RF
+from algorithms import KNN_MLP
 import split_data
 
 
@@ -42,23 +42,37 @@ def missing_data_table():
 
 test_value = 0.5
 #################################### KNN IMPUTATION ####################################
-std_data_impt_knn = KNN_MLP.imputation(std_data, 5, "Standardized_Data")
+# std_data_impt_knn = KNN_MLP.imputation(std_data, 5, "Standardized_Data")
+std_data_impt_knn = 'output/Imputed_Standardized_Data_using_KNN.csv'
 X_train_std_knn, y_train_std_knn, X_test_std_knn, y_test_std_knn = split_data.split(pd.read_csv(std_data_impt_knn), test_value)
 
-data_impt_knn = KNN_MLP.imputation(data, 5, 'Data')
+# data_impt_knn = KNN_MLP.imputation(data, 5, 'Data')
+data_impt_knn = 'output/Imputed_Data_using_KNN.csv'
 X_train_knn, y_train_knn, X_test_knn, y_test_knn = split_data.split(pd.read_csv(data_impt_knn), test_value)
 #################################### GA IMPUTATION ####################################
-std_data_impt_ga = GA.imputation_std_data(std_data, "Standardized_Data", test_value)
+# std_data_impt_ga = GA.imputation_std_data(std_data, "Standardized_Data", test_value)
+std_data_impt_ga = 'output/Imputed_Standardized_Data_using_GA.csv'
 X_train_std_ga, y_train_std_ga, X_test_std_ga, y_test_std_ga = split_data.split(pd.read_csv(std_data_impt_ga), test_value)
 
-data_impt_ga = GA.imputation_data(data, 'Data', test_value)
+# data_impt_ga = GA.imputation_data(data, 'Data', test_value)
+data_impt_ga = 'output/Imputed_Data_using_GA.csv'
 X_train_ga, y_train_ga, X_test_ga, y_test_ga = split_data.split(pd.read_csv(data_impt_ga), test_value)
 #################################### MICE IMPUTATION ####################################
-std_data_impt_mice = MICE_RF.imputation_with_mice_rf(std_data, "Standardized_Data")
+# std_data_impt_mice = MICE_RF.imputation(std_data, "Standardized_Data")
+std_data_impt_mice = 'output/Imputed_Standardized_Data_using_MICE.csv'
 X_train_std_mice, y_train_std_mice, X_test_std_mice, y_test_std_mice = split_data.split(pd.read_csv(std_data_impt_mice), test_value)
 
-data_impt_mice = MICE_RF.imputation_with_mice_rf(data, 'Data')
+# data_impt_mice = MICE_RF.imputation(data, 'Data')
+data_impt_mice = 'output/Imputed_Data_using_MICE.csv'
 X_train_mice, y_train_mice, X_test_mice, y_test_mice = split_data.split(pd.read_csv(data_impt_mice), test_value)
+#################################### ITERATIVE IMPUTER IMPUTATION ####################################
+# std_data_impt_itim = Iterative_Imputer.imputation(std_data, "Standardized_Data")
+std_data_impt_itim = 'output/Imputed_Standardized_Data_using_Iterative_Imputer.csv'
+X_train_std_itim, y_train_std_itim, X_test_std_itim, y_test_std_itim = split_data.split(pd.read_csv(std_data_impt_itim), test_value)
+
+# data_impt_itim = Iterative_Imputer.imputation(data, 'Data')
+data_impt_itim = 'output/Imputed_Data_using_Iterative_Imputer.csv'
+X_train_itim, y_train_itim, X_test_itim, y_test_itim = split_data.split(pd.read_csv(data_impt_itim), test_value)
 
 def get_content():
     content = html.Div([html.Div(children=[html.P(["Using 'water potability' dataset to study the different types of optimisation and classification methods,",html.Br(),
@@ -89,7 +103,12 @@ def get_content():
                                                     className="cleanTest"),
                                             html.Div(children=[html.Label(["Escape Optimization"])], className="cleanTest"),
                                             html.Div(children=[html.Label(["HHO Algorithm"])], className="cleanTest"),
-                                            html.Div(children=[html.Label(["SMA Algorithm"])], className="cleanTest")],
+                                            html.Div(children=[html.Label(["Iterative Imputer"]),
+                                                               html.Div(children=[dcc.Graph(figure=KNN_MLP.classifying_data_KNN_bar(X_train_std_itim, y_train_std_itim, X_test_std_itim, y_test_std_itim, test_value)),
+                                                                                    dcc.Graph(figure=KNN_MLP.classifying_data_KNN_pie(X_train_std_itim, y_train_std_itim, X_test_std_itim, y_test_std_itim))]),
+                                                                html.Div(children=[dcc.Graph(figure=KNN_MLP.classifying_data_MLP_bar(X_train_std_itim, y_train_std_itim, X_test_std_itim, y_test_std_itim, test_value)),
+                                                                                    dcc.Graph(figure=KNN_MLP.classifying_data_MLP_pie(X_train_std_itim, y_train_std_itim, X_test_std_itim, y_test_std_itim))])],
+                                                    className="cleanTest")],
                                 className="standardized-data"),
                         html.Div(children=[html.Label(children=["Normal Data"], id="nrml-label", className="title"),
                                             html.Div(children=[html.Label(["KNN Imputation"]),
@@ -112,9 +131,13 @@ def get_content():
                                                     className="cleanTest"),
                                             html.Div(children=[html.Label(["Escape Optimization"])], className="cleanTest"),
                                             html.Div(children=[html.Label(["HHO Algorithm"])], className="cleanTest"),
-                                            html.Div(children=[html.Label(["SMA Algorithm"])], className="cleanTest")],
-                                className="normal-data"),
-                        html.Div(children=[html.Label(children=["Comparision"], className="title")], className='comparision')],
+                                            html.Div(children=[html.Label(["Iterative Imputer"]),
+                                                               html.Div(children=[dcc.Graph(figure=KNN_MLP.classifying_data_KNN_bar(X_train_itim, y_train_itim, X_test_itim, y_test_itim, test_value)),
+                                                                                    dcc.Graph(figure=KNN_MLP.classifying_data_KNN_pie(X_train_itim, y_train_itim, X_test_itim, y_test_itim))]),
+                                                                html.Div(children=[dcc.Graph(figure=KNN_MLP.classifying_data_MLP_bar(X_train_itim, y_train_itim, X_test_itim, y_test_itim, test_value)),
+                                                                                    dcc.Graph(figure=KNN_MLP.classifying_data_MLP_pie(X_train_itim, y_train_itim, X_test_itim, y_test_itim))])],
+                                                    className="cleanTest")],
+                                className="normal-data")],
             className="main-content")
 
     return content
